bca9ac7df6528e2acdeee95dcf62891f7169c7d0
OlivierVillequey <oliviervillequey@gmail.com>
2015-06-09 16:46:25 +0900

    modification of functions and execution

--- app.py
+++ app.py
@@ -14,11 +14,11 @@ NEGATIVE = set(['not','n\'t','nothing','nobody','never','no'])
 #list of words for double negatives using prefixes
 NEGATIVE_PREFIXES = set()
 #list of words for double negatives using bad grammar
-BAD_GRAMMAR = set(['nothing','never','no'])
+BAD_GRAMMAR = set(['nothing','never','no','ai','not'])
 
 # populate the negative prefixes set
 wordlist = [w for w in nltk.corpus.words.words('en') if w.islower()]
-for w in [w for w in wordlist if re.search('^(in|un|im|non|ir)', w)]:
+for w in [w for w in wordlist if re.search('^(in|un|im|non|ir).+', w)]:
     NEGATIVE_PREFIXES.add(w)
 
 class DblNegatives(object):
@@ -31,8 +31,8 @@ class DblNegatives(object):
         tokens = word_tokenize(sentence)
         # check the number of negative meaning words
         for t in tokens:
-            if t.lower() in NEGATIVE or t.lower() in NEGATIVE_WORDS or t.lower() in NEGATIVE_PREFIXES:
-                #print 'FOUND A NEGATIVE WORD, IT WAS:', t
+            if (t.lower() in NEGATIVE) or (t.lower() in NEGATIVE_WORDS) or (t.lower() in NEGATIVE_PREFIXES):
+                #print ('FOUND A NEGATIVE WORD, IT WAS:', t)
                 neg_count += 1
         if neg_count > 1 and neg_count % 2 == 0:
             print("This is a sentence with a double negative.")
@@ -56,6 +56,10 @@ class DblNegatives(object):
         for word in sent:
             if len(word)>1:
                 #change n't into not
+                if word == 'wo':
+                    word == 'will'
+                if word == 'ca':
+                    word = 'can'
                 if word == 'n\'t':
                     word = 'not'
                 #Here we change every negative word into his corresponding good word
@@ -81,6 +85,7 @@ class DblNegatives(object):
         new_sent = []
         for word in sent:
             pass
+        #print (new_sent+"\n")
 
 
     # check if it is a double negative sentence using bad grammar
@@ -100,27 +105,35 @@ class DblNegatives(object):
             return 'ever'
         elif word == 'no':
             return 'any'
+        elif word == 'not':
+            return ''
 
     # correct the double negative sentence using bad grammar
     def correct_bad_grammar_sentence(self, sent):
         tokens = word_tokenize(sent)
         tagged = nltk.pos_tag(tokens)
+        modification = 0
         new_sent = []
         #use tags to spot good words to change
         for (word,tag) in tagged:
             if len(word)>1:
                 if word == 'wo':
                     word = 'will'
+                if word == 'ai':
+                    word = 'don\'t'
+                if word == 'ca':
+                    word = 'can'
                 if word == 'n\'t':
                     word = 'not'
-                if (tag == 'NN' or tag == 'DT') and word in BAD_GRAMMAR:
+                if (tag == 'NN' or tag == 'DT' or tag == 'RB') and (word in BAD_GRAMMAR) and modification == 0:
                     new_sent.append (self.change_bad_grammar(word))
+                    modification = 1
                 else:
                     new_sent.append(word)
             else:
                     new_sent.append(word)
         new_sent = ' '.join(new_sent)
-        print (new_sent)
+        print (new_sent+"\n")
 
     # processes a sentence and checks if it has a double negative
     # if so, corrects the double negative
@@ -135,12 +148,17 @@ class DblNegatives(object):
             elif self.is_bad_grammar_sentence(sentence):
                 self.correct_bad_grammar_sentence(sentence)
         else:
-            print ('This sentence does not contain a double negative.')
+            print ('This sentence does not contain a double negative.'+"\n")
 
 
 def main():
     dn = DblNegatives()
-
+    print('*/----------------------------------------------------------------*/')
+    print('*/CS372 NLP project: Double negatives')
+    print('*/objective: correcting sentences with double negative to make')
+    print('*/easier to understand.')
+    print('*/----------------------------------------------------------------*/')
+          
     # read test sentences from a text file
     sentences = [line.rstrip('\n') for line in open('sentences.txt')]
     for sentence in sentences:
